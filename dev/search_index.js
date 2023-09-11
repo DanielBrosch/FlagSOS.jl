aa0e0f69ce1b6391360ae272ec8da201c94cf559
Documenter.jl <documenter@juliadocs.github.io>
2023-09-11 12:18:41 +0000

    build based on 195a2dc

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FlagSOS","category":"page"},{"location":"#FlagSOS","page":"Home","title":"FlagSOS","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FlagSOS.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FlagSOS]","category":"page"}]
+}
