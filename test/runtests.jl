d12fb4b9e7b7d5a26b315a2ce52385422199979c
Daniel Brosch <73886037+DanielBrosch@users.noreply.github.com>
2023-09-14 14:17:28 +0200

    add examples to testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,36 @@ using Test
 using Aqua
 using Documenter
 
+
+"""
+    _include_sandbox(filename)
+Include the `filename` in a temporary module that acts as a sandbox. (Ensuring
+no constants or functions leak into other files.)
+"""
+function _include_sandbox(filename)
+    mod = @eval module $(gensym()) end
+    return Base.include(mod, filename)
+end
+
+function _file_list(full_dir, relative_dir, extension)
+    return map(
+        file -> joinpath(relative_dir, file),
+        filter(file -> endswith(file, extension), sort(readdir(full_dir))),
+    )
+end
+
+function _test_directory(dir)
+    rm.(_file_list(dir, dir, ".md"))
+    for filename in _file_list(dir, dir, ".jl")
+        # `include` the file to test it before `#src` lines are removed. It is
+        # in a testset to isolate local variables between files.
+        Test.@testset "$(filename)" begin
+            _include_sandbox(filename)
+        end
+    end
+    return nothing
+end
+
 @testset "FlagSOS.jl" begin
     @testset "Code quality (Aqua.jl)" begin
         Aqua.test_all(FlagSOS; ambiguities = (imported = false))
@@ -11,5 +41,9 @@ using Documenter
     @testset "Doctests" begin
         doctest(FlagSOS)
     end
-    # Write your tests here.
+
+    @testset "Examples" begin
+        _test_directory.(joinpath(@__DIR__, "..", "docs", "src", "examples"))
+    end
 end
+
